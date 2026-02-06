0cb0800e981e583d5037b6aa778f0c0633385261
gagarwal2003 <114745980+gagarwal2003@users.noreply.github.com>
2026-02-06 14:35:03 +0530

    Update ddl_bronze.sql

--- scripts/Broze/ddl_bronze.sql
+++ scripts/Broze/ddl_bronze.sql
@@ -1,1 +1,81 @@
+/*
+=============================================================
+Create Database and Schemas
+=============================================================
+Script Purpose:
+    This script creates tables in the 'bronze' schema , dropping existing tables if they already exist.
+Run This script to re-define the DDL structure of 'Bronze' Tables
+	
+WARNING:
+    t.
+*/
+if object_id('bronze.crm_cust_info','U') is not null
+	DROP table bronze.crm_cust_info;
+
+create table bronze.crm_cust_info (
+cst_id int,
+cst_key varchar(50),
+cst_firstname varchar(50),
+cst_lastname varchar(50),
+cst_material_status varchar(50),
+cst_gndr varchar(50),
+cst_create_date DATE
+);
+
+if object_id('bronze.crm_prd_info','U') is not null
+	DROP table bronze.crm_prd_info;
+
+create table bronze.crm_prd_info (
+prd_id int,
+prd_key varchar(50),
+prd_nm varchar(50),
+prd_cost varchar(50),
+prd_line varchar(50),
+prd_start_dt DATETIME,
+prd_end_dt DATETIME);
+
+if object_id('bronze.crm_sales_details','U') is not null
+	DROP table bronze.crm_sales_details;
+
+create table bronze.crm_sales_details(
+sls_ord_num varchar(50),
+sls_prd_key varchar(50),
+sls_cust_id int,
+sls_order_dt int,
+sls_ship_dt int,
+sls_due_dt int,
+sls_sales int,
+sls_quantity int,
+sls_price int);
+
+if object_id('bronze.erp_cust_az12','U') is not null
+	DROP table bronze.erp_cust_az12;
+
+
+create table bronze.erp_cust_az12(
+cid varchar(50),
+bdate DATE,
+gen varchar(50)
+);
+
+if object_id('bronze.erp_loc_a101','U') is not null
+	DROP table bronze.erp_loc_a101;
+
+create table bronze.erp_loc_a101(
+cid varchar(50),
+cntry varchar(50)
+);
+
+if object_id('bronze.erp_px_cat_g1v2','U') is not null
+	DROP table bronze.erp_px_cat_g1v2;
+
+
+create table bronze.erp_px_cat_g1v2(
+id varchar(50),
+cat varchar(50),
+subcat varchar(50),
+maintenance varchar(50)
+);
+
+
 
